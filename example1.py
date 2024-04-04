8aee8b285d7e9c6a6705310c4456f25a0ad918f0
Sebastiaan <bas.terkeurs@gmail.com>
2024-04-04 12:54:35 -0700

    initial commit

--- example1.py
+++ example1.py
@@ -35,5 +35,5 @@ If we want to combine variables with literal text output, you need to use format
 Note the inclusion of the f in front of the quotation marks.  We can then just include the 
 variable in the {} where we would like to show the value of that variable
 '''
-x = 2.5
-print(f"The value of x is {x}")
+x = "Hello world"
+print(f"and then he said \'{x}\'")
